b0847c28176b025fc72bf0fd62d6051d92d94121
Samuel.fp <samuel.fernandepeinado@gmail.com>
2024-11-17 12:14:04 +0100

    feat(space_invader): :sparkles: Add settings, bg_color and window size

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -1,13 +1,17 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class SpaceInvaders:
     """Main class to control thegame behavior"""
 
     def __init__(self):
         pygame.init()
+        self.setting = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.setting.screen_width, self.setting.screen_height))
         pygame.display.set_caption("Space invaders")
 
     def run_game(self):
@@ -16,6 +20,8 @@ class SpaceInvaders:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            self.screen.fill(self.setting.bg_color)
 
             pygame.display.flip()
 
