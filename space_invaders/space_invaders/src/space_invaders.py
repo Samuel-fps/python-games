480411237fd829c1824018a0163a92bdc0cd1ffa
Samuel.fp <samuel.fernandepeinado@gmail.com>
2024-11-17 18:44:15 +0100

    feat(space_invader): :sparkles: Ship movement

--- space_invaders/space_invaders/src/space_invaders.py
+++ space_invaders/space_invaders/src/space_invaders.py
@@ -9,10 +9,11 @@ class SpaceInvaders:
 
     def __init__(self):
         pygame.init()
-        self.setting = Settings()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.setting.screen_width, self.setting.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Space invaders")
 
         self.ship = Ship(self)
@@ -22,21 +23,42 @@ class SpaceInvaders:
         """Game loop"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
             pygame.display.flip()
 
     def _check_events(self):
-        """cCheck inputs"""
+        """Check inputs"""
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """Keydown controller"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+            
+        
+    def _check_keyup_events(self, event):
+        """Keyup controller"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+            
     def _update_screen(self):
         """Update game screen"""
-        self.screen.fill(self.setting.bg_color)
+        self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
 
 if __name__ == '__main__':
     si = SpaceInvaders()
